ce473df92978a2ba65ce8eddda3ab2c47cae7e3a
Silvan Sholla <42264989+Skidrow17@users.noreply.github.com>
2022-11-16 21:18:37 +0200

    Update Constants.js

--- delta_deploy/util/Constants.js
+++ delta_deploy/util/Constants.js
@@ -1,6 +1,5 @@
 module.exports.META_XML = '-meta.xml';
 module.exports.FOLDER_SEPARATOR = '/';
-module.exports.STANDARD_PACKAGE_VERSION = '52.0';
 module.exports.SOURCE_FOLDER = 'force-app/main/default';
 module.exports.DESTINATION_FOLDER = 'delta_deploy/force-app/main/default';
 
